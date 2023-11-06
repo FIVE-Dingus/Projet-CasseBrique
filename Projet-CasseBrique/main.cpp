aa7c2d008ed59cc4e0dd0c9c1c5149a63a4dc4be
TranquillouMAN <beney7189@gmail.com>
2023-11-06 13:10:41 +0100

    sfml fonctionne tjrs pas

--- Projet-CasseBrique/main.cpp
+++ Projet-CasseBrique/main.cpp
@@ -1,24 +1,45 @@
 #include <SFML/Graphics.hpp>
-using namespace sf;
 
-int main()
+int main(int argc, char** argv)
 {
-    RenderWindow window(VideoMode(200, 200), "SFML works!");
-    CircleShape shape(100.f);
-    shape.setFillColor(Color(255, 255, 255));
+    //Création d'une fenêtre
+    sf::RenderWindow oWindow(sf::VideoMode(640, 480), "SFML");
 
-    while (window.isOpen())
+    //Création d'un cercle de radius 100
+    sf::CircleShape oCircle(100.f);
+    //A la position 0, 0
+    oCircle.setPosition(0.f, 0.f);
+    //Et de couleur verte
+    oCircle.setFillColor(sf::Color::Green);
+
+
+    //Création d'un rectangle de taille 50, 50
+    sf::RectangleShape oRectangle(sf::Vector2f(50.f, 50.f));
+    //A la position 100, 100
+    oCircle.setPosition(100.f, 100.f);
+    //Et de couleur rouge
+    oRectangle.setFillColor(sf::Color::Red);
+
+    //GameLoop
+    while (oWindow.isOpen())
     {
-        Event event;
-        while (window.pollEvent(event))
+        //EVENT
+        sf::Event oEvent;
+        while (oWindow.pollEvent(oEvent))
         {
-            if (event.type == Event::Closed)
-                window.close();
+            if (oEvent.type == sf::Event::Closed)
+                oWindow.close();
         }
 
-        window.clear();
-        window.draw(shape);
-        window.display();
+        //UPDATE
+
+        //DRAW
+        oWindow.clear();
+
+        oWindow.draw(oCircle);
+        oWindow.draw(oRectangle);
+
+        oWindow.display();
     }
 
     return 0;
