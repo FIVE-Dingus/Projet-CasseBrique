078e18ba74898633aff8914cd87c280522a46b25
Gwenitora <gwenitora.gardu74@gmail.com>
2023-11-15 16:55:20 +0100

    colors

--- Project-CasseBrique/main.cpp
+++ Project-CasseBrique/main.cpp
@@ -8,19 +8,27 @@ using namespace sf;
 
 void Main::initGameObjects()
 {
-    for (int i = 65; i < 750; i += 75) {
-
-        this->newGameObjects(new GameObject(Vect2(i, 100), Vect2(60, 20), MyColor(0xfff000)));
-        this->newGameObjects(new GameObject(Vect2(i, 130), Vect2(60, 20), MyColor(0xff0000)));
-        this->newGameObjects(new GameObject(Vect2(i, 160), Vect2(60, 20), MyColor(0x00ff00)));
-        this->newGameObjects(new GameObject(Vect2(i, 190), Vect2(60, 20), MyColor(0xfff0ff)));
-        this->newGameObjects(new GameObject(Vect2(i, 220), Vect2(60, 20), MyColor(0xff0ff0)));
+    MyColor col1(0xff0000);
+    MyColor col2(0x00ff00);
+    MyColor col3(0x0000ff);
+    MyColor col4(0xffffff);
+    MyColor mix1;
+    MyColor mix2;
+    MyColor mix;
+    for (int i = 0; i < 10; i++) {
+        col1.mixin(col2, &mix1, i / 9.f);
+        col3.mixin(col4, &mix2, i / 9.f);
+        for (int j = 0; j < 10; j++)
+        {
+            mix1.mixin(mix2, &mix, j / 9.f);
+            this->newGameObjects(new GameObject(Vect2(75 * i + 65, 30 * j + 100), Vect2(60, 20), mix));
+        }
     }
     this->newGameObjects(new Ball(Vect2(400, 700), 10, MyColor(0xffffff)));
     this->newGameObjects(new MyRect(Vect2(400, 700), Vect2(60, 20), MyColor(0xff0ff0)));
-    this->allGameObjects[51]->activeCollision();
-    this->allGameObjects[51]->setSubdivisionOrigin({ 60, 2 });
-    this->allGameObjects[51]->setOrigin({ 15, 1 });
+    this->allGameObjects[101]->activeCollision();
+    this->allGameObjects[101]->setSubdivisionOrigin({ 60, 2 });
+    this->allGameObjects[101]->setOrigin({ 15, 1 });
 
 }
 
